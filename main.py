18589ad2df8b2d58d28b414d04e4a2e9edd72b82
Samusoidal <info@samusoidal.com>
2018-05-27 17:24:01 -0700

    Added recpient modification support

--- main.py
+++ main.py
@@ -84,6 +84,27 @@ def save_settings():
     settings_file.write(str_server.get() + '\n' + str_username.get() + '\n' + str_password.get() + '\n');
     settings_file.close();
 
+def save_address():
+    name = lsvar_current_address.get();
+    if name != "" and name != "{'None'}":
+        odata = db_addresses[name];
+        data = {}
+        data['name'] = str_rname.get();
+        data['address'] = str_raddress.get();
+        data['enabled'] = True;
+        r = requests.put('http://' + str_server.get() + '/recipients/' + str(odata['rid']), auth=(str_username.get(), str_password.get()), json={ 'name': data['name'], 'address': data['address'], 'enabled': data['enabled'] });
+    else:
+        save_address_as_new();
+
+def save_address_as_new():
+    name = lsvar_current_address.get();
+    odata = db_addresses[name];
+    data = {}
+    data['name'] = str_rname.get();
+    data['address'] = str_raddress.get();
+    data['enabled'] = True;
+    r = requests.post('http://' + str_server.get() + '/recipients', auth=(str_username.get(), str_password.get()), json={ 'name': data['name'], 'address': data['address'], 'enabled': data['enabled'] });
+
 # GUI
 mw = ttk.Notebook(root, width=300, height=300);
 
@@ -124,15 +145,15 @@ ls_clients.grid(row=1,column=1);
 client_page_f1.grid(row=1,column=1);
 
 # RECIPIENT PAGE
-address_page = Frame(mw, padx=10, pady=10);
+address_page = Frame(mw, padx=2, pady=2);
 
-address_page_f1 = Frame(address_page, padx=10, pady=10);
+address_page_f1 = Frame(address_page, padx=4, pady=10);
 ls_addresses = OptionMenu(address_page_f1, lsvar_current_address, {"None"});
 lsvar_current_address.trace('w', dropdown_change_recipient);
-ls_addresses.grid(row=1,column=1);
+ls_addresses.grid(row=1,column=2);
 address_page_f1.grid(row=1,column=1,sticky=W);
 
-address_page_f2 = Frame(address_page);
+address_page_f2 = Frame(address_page, padx=4, pady=10);
 label_rname = Label(address_page_f2, text="Name");
 input_rname = Entry(address_page_f2, textvariable=str_rname);
 label_rname.grid(row=1,column=1);
@@ -142,7 +163,14 @@ label_raddress = Label(address_page_f2, text="Address");
 input_raddress = Entry(address_page_f2, textvariable=str_raddress);
 label_raddress.grid(row=2,column=1);
 input_raddress.grid(row=2,column=2);
-address_page_f2.grid(row=2,column=1);
+address_page_f2.grid(row=2,column=1,sticky=W);
+
+address_page_f3 = Frame(address_page, padx=4, pady=10);
+button_saveAddress = Button(address_page_f3, text="Save", command=save_address);
+button_saveAddress.grid(row=1,column=1);
+button_newAddress = Button(address_page_f3, text="Save As New", command=save_address_as_new);
+button_newAddress.grid(row=1,column=2);
+address_page_f3.grid(row=3,column=1,sticky=W);
 
 mw.add(config_page, text="Config");
 mw.add(client_page, text="Clients");
